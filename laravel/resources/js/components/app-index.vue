25613d59dac7dbbca7ee11834f83219337942cc9
JK <jayedulk@gmail.com>
2025-05-06 20:25:09 +0600

    Initial wrapper setup

--- laravel/resources/js/components/app-index.vue
+++ laravel/resources/js/components/app-index.vue
@@ -1,11 +1,17 @@
 <template>
-	<div>
-		Hello Vue
-	</div>
+	<PortraitRoot>
+		<div class="h-full w-full bg-white">
+			Hi
+		</div>
+	</PortraitRoot>
 </template>
 
 <script>
+	import PortraitRoot from './portrait-root.vue';
+
 	export default {
-		
+		components: {
+			PortraitRoot
+		}
 	}
 </script>
--- laravel/resources/views/index.blade.php
+++ laravel/resources/views/index.blade.php
@@ -11,7 +11,7 @@
             @vite(['resources/css/app.css', 'resources/js/app.js'])
         @endif
     </head>
-    <body>
+    <body style="background-color: black">
         <div id="app"></div>
     </body>
 </html>
